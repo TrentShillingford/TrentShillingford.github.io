8e75f4f8a36767bd557d7e8175a5c4893117ec3c
Travis <tshillingford1@gmail.com>
2021-11-22 11:33:18 -0500

    more changes

--- elements/sidebar.js
+++ elements/sidebar.js
@@ -4,5 +4,6 @@ sidebar.style.width = '110px';
 sidebar.style.height = window.screen.availHeight - Math.abs(window.innerHeight - window.outerHeight);
 sidebar.style.position = 'fixed';
 sidebar.style.zIndex = "0";
+sidebar.style.setProperty('-webkit-backface-visibility', 'hidden');
 sidebar.id = "sidebar";
 document.getElementById("veil").insertBefore(sidebar, document.getElementById("veil").firstChild)
